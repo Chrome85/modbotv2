0db9b6163fd89afca628e131cf555d91eb4fa5dd
Chrome85 <75063366+Chrome85@users.noreply.github.com>
2021-04-23 18:21:33 +0300

    Delete shard.js

--- shard.js
+++
@@ -1,6 +0,0 @@
-const {ShardingManager} = require ('discord.js');
-const manager = new ShardingManager ('./index.js', {totalShards: 'auto',
-token:'ODE4ODI1MTMzNTgxOTI2NDIw.YEdsVA.5yn7qAZYwmBMhTYYRGIX8jVS7mY'}); // TOKEN discord.com/developers Alabilirsiniz!
-
-manager.on ('shardCreate', shard => console.log (`Shardlar Başlatılıyor!`));
-manager.spawn ();
